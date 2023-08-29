8943002d68f54f9389182a1da07bebf03e0e7c54
ciannella <lucasciannella@hotmail.com>
2023-08-29 20:59:29 -0300

    Iniciando projeto, configurações iniciais de docker-compose e banco de dados.

---
+++ src/main/java/com/vloditech/control/ControlApplication.java
@@ -0,0 +1,13 @@
+package com.vloditech.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vloditech/control/ControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vloditech.control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
